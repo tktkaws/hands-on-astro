20fbf9ebbfb11c7f716d01b94ffb992e5d6515a5
tktkaws <ttkaws15@gmail.com>
2024-03-11 17:01:02 +0900

    Update Layout.astro and newt.ts, add [slug].astro and update index.astro

--- src/lib/newt.ts
+++ src/lib/newt.ts
@@ -1,5 +1,12 @@
 import { createClient } from 'newt-client-js'
 
+export interface Article {
+    title: string
+    slug: string
+    body: string
+}
+
+
 export const newtClient = createClient({
     spaceUid: import.meta.env.NEWT_SPACE_UID,
     token: import.meta.env.NEWT_CDN_API_TOKEN,
