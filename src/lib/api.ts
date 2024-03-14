e916719e342139a32614456d2bb372e0f32d9298
tktkaws <ttkaws15@gmail.com>
2024-03-14 16:25:28 +0900

    価格から記事一覧を取得

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 // import { createClient } from 'newt-client-js'
 import { newtClient } from "../lib/newt";
+import type { Article, Tag } from "../lib/newt";
 
 // タグから記事一覧を取得する
 export const getArticleByTags = async (tagId: string) => {
@@ -16,3 +17,20 @@ export const getArticleByTags = async (tagId: string) => {
 	const items = tagedArticles.items;
 	return items;
 };
+
+// 価格から記事一覧を取得する
+export const getArticleByPrice = async (price: string) => {
+	console.log('price:', price);
+	const Articles = await newtClient.getContents<Article>({
+		appUid: "blog",
+		modelUid: "article",
+		query: {
+			select: ["title", "slug", "body", "tags", "coverImage", "price"],
+			price: price,
+		},
+	},);
+	console.log('Articles:', Articles);
+
+	const items = Articles.items;
+	return items;
+}
