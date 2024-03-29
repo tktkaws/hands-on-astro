acfc415bc3f3557d58c2626b246fe1d5a590f0ec
tktkaws <ttkaws15@gmail.com>
2024-03-29 12:49:59 +0900

    install swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import swup from '@swup/astro';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+
+  integrations: [tailwind(), swup({ theme: false })]
 });
